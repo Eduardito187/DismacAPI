f9667f02f5d525b4ea7a74fe36c04f24e66fb478
Eduardito187 <eduardchavez302@gmail.com>
2022-12-21 15:44:36 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"><h1 style='background-color: white;'>Toxica MARICA </h1></a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
@@ -36,7 +36,6 @@
                 </div>
             @endif
             <?php
-            echo "<h1>HOLA </h1>".date("Y-m-d H:i:s");
             ?>
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
