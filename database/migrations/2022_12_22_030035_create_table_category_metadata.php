7277112fc2301e4355d08b9d29744dea110502fe
Eduardito187 <eduardchavez302@gmail.com>
2022-12-22 03:01:16 -0400

    Create 2022_12_22_030035_create_table_category_metadata.php

---
+++ database/migrations/2022_12_22_030035_create_table_category_metadata.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('category', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_metadata');
+            $table->foreign('id_metadata')->references('id')->on('metadata')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('id_metadata');
+        });
+    }
+};
