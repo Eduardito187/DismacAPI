5672dd32f61ef4f1b00f31b34047ef469ace95b4
Eduardito187 <eduardchavez302@gmail.com>
2022-12-25 07:20:14 -0400

    Update 2022_12_25_070836_create_table_example.php

--- database/migrations/2022_12_25_070836_create_table_example.php
+++ database/migrations/2022_12_25_070836_create_table_example.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('table_example', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
         });
     }
 
