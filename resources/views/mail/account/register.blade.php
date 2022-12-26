5eed5ea05ee8d7ec19192e971b99c133a0b3f608
Eduardito187 <eduardchavez302@gmail.com>
2022-12-25 20:45:58 -0400

    mail

--- resources/views/mail/account/register.blade.php
+++ resources/views/mail/account/register.blade.php
@@ -7,16 +7,16 @@
     <title>Register Account | <?php echo env("APP_NAME"); ?></title>
 </head>
 <body>
-    <div style="width: 100%;">
-        <div style="width: 100%;display: flex;text-align: center;">
-            <div style="display: inline-block;padding: 5px;">
-                <img style="width: 50px;" src="{{url('storage/fbnew.png')}}" />
+    <div class="column">
+        <div class="rowCenter">
+            <div class="itemRowFlex">
+                <img class="imgSocial" src="{{url('storage/fbnew.png')}}" />
             </div>
-            <div style="display: inline-block;padding: 5px;">
-                <img style="width: 50px;" src="{{url('storage/instanew.png')}}" />
+            <div class="itemRowFlex">
+                <img class="imgSocial" src="{{url('storage/instanew.png')}}" />
             </div>
-            <div style="display: inline-block;padding: 5px;">
-                <img style="width: 50px;" src="{{url('storage/ytnew.png')}}" />
+            <div class="itemRowFlex">
+                <img class="imgSocial" src="{{url('storage/ytnew.png')}}" />
             </div>
         </div>
     </div>
