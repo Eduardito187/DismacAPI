d430d33f59a149e172bc73394ca0acfac8cbecc2
Eduardito187 <eduardchavez302@gmail.com>
2023-01-12 01:09:42 -0400

    Update SendCode.php

--- app/Http/Controllers/Api/Mail/SendCode.php
+++ app/Http/Controllers/Api/Mail/SendCode.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\Mail;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Classes\Helper\MailCode;
 use App\Mail\Code;
 use Mail as Mailer;
 
@@ -31,10 +30,6 @@ class SendCode extends Controller
         $response = array();
         if (!is_null($request->all()["email"]) && !is_null($request->all()["code"])) {
             Mailer::to($request->all()["email"])->send(new Code($request->all()["code"]));
-            /*
-            $newEmail = new MailCode($request->all()["email"], "Código de verificación", $request->all()["code"]);
-            $newEmail->createMail();
-            */
             $response = array("status" => true);
         }else{
             $response = array("status" => false);
