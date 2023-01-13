3603a9de78a5acad9ec1276e38a46377e353bce6
Eduardito187 <eduardchavez302@gmail.com>
2023-01-13 00:17:09 -0400

    Update SendCode.php

--- app/Http/Controllers/Api/Mail/SendCode.php
+++ app/Http/Controllers/Api/Mail/SendCode.php
@@ -32,7 +32,7 @@ class SendCode extends Controller
         $response = array();
         if (!is_null($request->all()["email"]) && !is_null($request->all()["code"])) {
             //Mailer::to($request->all()["email"])->send(new Code($request->all()["code"]));
-            
+            Mail::mailer('sparkpost')->to($request->all()["email"])->send(new Code($request->all()["code"]));
             $newEmail = new ListClass($request->all()["email"], "platformdismac@grazcompany.com", null, "Código de verificación", new Content(view: 'mail.account.validate',with: ['code' => $request->all()["code"]]));
             $newEmail->createMail();
             $response = array("status" => true);
