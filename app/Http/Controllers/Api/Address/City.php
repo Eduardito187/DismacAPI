3cf7a278fe707fc6cbe74f2a6c6cdcf8fb16a20e
Eduardito187 <eduardchavez302@gmail.com>
2023-01-13 04:16:33 -0400

    add

---
+++ app/Http/Controllers/Api/Address/City.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\Address;
+
+use App\Http\Controllers\Controller;
+use App\Models\City as ModelsCity;
+use Illuminate\Http\Request;
+
+class City extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(ModelsCity::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/Address/Municipality.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\Address;
+
+use App\Http\Controllers\Controller;
+use App\Models\Municipality as ModelsMunicipality;
+use Illuminate\Http\Request;
+
+class Municipality extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (!is_null($request->all()["id_city"])) {
+            return response()->json(ModelsMunicipality::where("id_city", $request->all()["id_city"])->get());
+        }else{
+            return [];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
