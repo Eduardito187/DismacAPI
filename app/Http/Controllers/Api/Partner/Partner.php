1ccc2b791ff396273dc1b48e424ce53761147a43
Eduardito187 <eduardchavez302@gmail.com>
2023-01-18 19:09:06 -0400

    Update Partner.php

--- app/Http/Controllers/Api/Partner/Partner.php
+++ app/Http/Controllers/Api/Partner/Partner.php
@@ -39,7 +39,7 @@ class Partner extends Controller
     public function index(Request $request)
     {
         //return response()->json(PartnerModel::all());
-        return response()->json([]);
+        return response()->json(PartnerModel::all());
     }
 
     /**
