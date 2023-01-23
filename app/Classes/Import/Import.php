de4e9ef73085512dd11cc383e886453f179f488b
Eduardito187 <eduardchavez302@gmail.com>
2023-01-23 05:02:23 -0400

    day 4

---
+++ app/Classes/Import/Import.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Classes\Import;
+
+use App\Classes\Helper\Date;
+use App\Classes\Helper\Status;
+use App\Classes\Helper\Text;
+
+class Import{
+    CONST AUTH = "Wagento:wagento2021";
+    /**
+     * @var Date
+     */
+    protected $date;
+    /**
+     * @var Status
+     */
+    protected $status;
+    /**
+     * @var Text
+     */
+    protected $text;
+
+    public function __construct() {
+        $this->date     = new Date();
+        $this->status   = new Status();
+        $this->text     = new Text();
+    }
+
+    /**
+     * @param array $request
+     * @return array $response
+     */
+    public function importCategory(array $request){
+        
+        $url = 'https://posapi.dismac.com.bo/v2/Product/GetItems';
+        $data = [
+            "GrupoArticulo"  => $request["GrupoArticulo"],
+            "Disponibilidad" => $request["Disponibilidad"],
+            "Precios"        => $request["Precios"],
+            "SubCategoria"   => $request["SubCategoria"]
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HTTPGET, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json; charset=utf-8','Authorization: Basic '. base64_encode(SELF::AUTH)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response);
+    }
+}
+
+?>
--- app/Http/Controllers/Api/Import/Category.php
+++ app/Http/Controllers/Api/Import/Category.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Api\Import;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Classes\Import\Import;
 
 class Category extends Controller
 {
+    protected $import;
+    public function __construct() {
+        $this->import     = new Import();
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -25,24 +31,7 @@ class Category extends Controller
      */
     public function store(Request $request)
     {
-        $url = 'https://posapi.dismac.com.bo/v2/Product/GetItems';
-        $data = [
-            "GrupoArticulo"  => 119,
-            "Disponibilidad" => true,
-            "Precios"        => true,
-            "SubCategoria"   => 0
-        ];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_HTTPGET, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json; charset=utf-8','Authorization: Basic '. base64_encode("Wagento:wagento2021")));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return response()->json(json_decode($result));
+        return response()->json($this->import->importCategory($request->all()));
     }
 
     /**
