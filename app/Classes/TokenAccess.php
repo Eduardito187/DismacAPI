02b31e6bac64762944a2a9ecd260c58b9e337a60
Eduardito187 <eduardchavez302@gmail.com>
2023-01-23 22:30:13 -0400

    Update TokenAccess.php

--- app/Classes/TokenAccess.php
+++ app/Classes/TokenAccess.php
@@ -24,7 +24,7 @@ class TokenAccess{
     }
 
     private function getTokenAccount(){
-        $validateAccount = ModelAccount::select('id')->where('token', $this->token)->get()->toArray();
+        $validateAccount = ModelAccount::select('id')->where('email', base64_decode($this->token))->get()->toArray();
         Log::debug("Tokens => ".json_encode($validateAccount));
         if (count($validateAccount) == 0) {
             return false;
