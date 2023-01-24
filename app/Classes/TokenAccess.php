7e8f572da6fbd4d7494bad4f3dd90b93f10f5dab
Eduardito187 <eduardchavez302@gmail.com>
2023-01-23 22:39:31 -0400

    Update TokenAccess.php

--- app/Classes/TokenAccess.php
+++ app/Classes/TokenAccess.php
@@ -15,6 +15,7 @@ class TokenAccess{
 
     public function validateAPI() {
         $validateAPIS = ModelIntegrations::select('id')->where('token', $this->token)->get()->toArray();
+        Log::debug("Tokens => ".json_encode($validateAPIS));
         if (count($validateAPIS) == 0) {
             $this->getTokenAccount();
         }else{
@@ -33,6 +34,7 @@ class TokenAccess{
 
     private function getTokenAccount(){
         $validateAccount = ModelAccount::select('id')->where('token', $this->getToken())->get()->toArray();
+        Log::debug($this->token." Email => ".$this->getToken()." => Tokens => ".json_encode($validateAccount));
         if (count($validateAccount) == 0) {
             return false;
         }else{
