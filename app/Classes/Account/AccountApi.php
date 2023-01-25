efc512a88679a626c03e41ad6e48432a6832425a
Eduardito187 <eduardchavez302@gmail.com>
2023-01-24 20:19:36 -0400

    Update AccountApi.php

--- app/Classes/Account/AccountApi.php
+++ app/Classes/Account/AccountApi.php
@@ -138,7 +138,7 @@ class AccountApi{
         $this->tokenAccess = new TokenAccess($request->header($this->text->getAuthorization()));
         $param = $request->all()["query"];
         $Accounts = Account::where("name", "like", "%".$param."%")->orWhere("email", "like", "%".$param."%")->
-        with(['account_partner' => function ($query) {
+        with(['accountPartner' => function ($query) {
             $query->where('id_partner', $this->tokenAccess->getToken());
         }])->get()->toArray();
         return $Accounts;
