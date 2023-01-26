e25c1b8f0eedad9227f179f523c87c0642017fbf
Eduardito187 <eduardchavez302@gmail.com>
2023-01-26 02:30:53 -0400

    day 5.31

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -29,6 +29,6 @@ class Account extends Model
     }
 
     public function accountStatus() {
-        return $this->hasOne(AccountLogin::class, 'id_account', 'id')->select("status");
+        return $this->belongsTo(AccountLogin::class, 'id_account', 'id')->select(['status']);
     }
 }
