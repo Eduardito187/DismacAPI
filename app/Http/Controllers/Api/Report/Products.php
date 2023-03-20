203a7bfef7bc6a889659c76755bf236333934241
Eduardito187 <eduardchavez302@gmail.com>
2023-03-20 01:09:51 -0400

    Update Products.php

--- app/Http/Controllers/Api/Report/Products.php
+++ app/Http/Controllers/Api/Report/Products.php
@@ -90,7 +90,7 @@ class Products extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
