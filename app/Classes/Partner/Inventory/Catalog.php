fe5693d714f4358c19a7cdcc72c09c21146eb78f
Eduardito187 <eduardchavez302@gmail.com>
2023-04-10 00:35:11 -0400

    Update Catalog.php

--- app/Classes/Partner/Inventory/Catalog.php
+++ app/Classes/Partner/Inventory/Catalog.php
@@ -157,7 +157,7 @@ class Catalog{
             $this->text->getCode() => $Catalog->code,
             $this->text->getCantidad() => count($UNIQUE),
             $this->text->getProducts() => $this->countProductsInCatalog($Catalog->id),
-            $this->text->getCategorias() => $NO_UNIQUE
+            $this->text->getCategorias() => $UNIQUE
         );
         //->distinct($this->text->getIdCategory())
     }
