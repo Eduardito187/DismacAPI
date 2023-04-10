094df6fa007b0f8169baca6c1fdbde0edd05b570
Eduardito187 <eduardchavez302@gmail.com>
2023-04-10 01:11:57 -0400

    Update Catalog.php

--- app/Classes/Partner/Inventory/Catalog.php
+++ app/Classes/Partner/Inventory/Catalog.php
@@ -150,7 +150,7 @@ class Catalog{
             throw new Exception($this->text->getCatalogNoExist());
         }
         $NO_UNIQUE = $Catalog->Categorias;
-        $UNIQUE = $Catalog->Categorias->groupBy(["id_category", "id_catalog"])->unique();
+        $UNIQUE = $Catalog->Categorias->groupBy("id_category")->unique();
         return array(
             $this->text->getId() => $Catalog->id,
             $this->text->getName() => $Catalog->name,
