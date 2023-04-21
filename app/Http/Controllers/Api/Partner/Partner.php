1c0da84574ee272d2ae770e8782ad7da88c17670
Eduardito187 <eduardchavez302@gmail.com>
2023-04-21 04:40:27 -0400

    Update Partner.php

--- app/Http/Controllers/Api/Partner/Partner.php
+++ app/Http/Controllers/Api/Partner/Partner.php
@@ -181,7 +181,7 @@ class Partner extends Controller
         $response = array();
         try {
             $Account = $this->accountApi->getAccountByToken($request->header($this->text->getAuthorization()));
-            $response = $this->text->getResponseApi($this->partnerApi->getCountStorePartner($Account->accountPartner->Partner), $this->text->getQuerySuccess());
+            $response = $this->text->getResponseApi($this->partnerApi->countSocialNetworkPartner($Account->accountPartner->Partner), $this->text->getQuerySuccess());
         } catch (Exception $th) {
             $response = $this->text->getResponseApi($this->status->getDisable(), $th->getMessage());
         }
