f858efe318f2a87e32fb043c0c35feee9d7f4f55
Eduardito187 <eduardchavez302@gmail.com>
2023-05-28 00:43:13 -0400

    Update AddressApi.php

--- app/Classes/Address/AddressApi.php
+++ app/Classes/Address/AddressApi.php
@@ -172,6 +172,7 @@ class AddressApi{
             Log::debug("######");
             return true;
         } catch (Exception $th) {
+            Log::debug("Error -> ".$th->getMessage());
             return false;
         }
     }
