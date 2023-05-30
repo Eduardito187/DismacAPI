6cb6b46dd676224e93471eeb407c88f17f98da54
Eduardito187 <eduardchavez302@gmail.com>
2023-05-30 00:06:42 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <style>
         @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
-        @import url('./../css//mail.css');
+        @import url('./../css/mail.css');
     </style>
 </head>
 
