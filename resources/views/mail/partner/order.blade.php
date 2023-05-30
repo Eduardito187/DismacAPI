0bcb5fe14f856a1fdb6b2689d9f53b89ebadfaff
Eduardito187 <eduardchavez302@gmail.com>
2023-05-30 01:29:10 -0400

    Update order.blade.php

--- resources/views/mail/partner/order.blade.php
+++ resources/views/mail/partner/order.blade.php
@@ -17,55 +17,63 @@ $SalesDetails = $Orden->SalesDetails;
     <style>
         @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+        body {
+            font-family: 'Muli', sans-serif;
+            color: rgba(0, 0, 0, 0.8);
+            font-weight: 400;
+            line-height: 1.58;
+            letter-spacing: -.003em;
+            font-size: 20px;
+            padding: 40px;
+        }
     </style>
-    <link rel="stylesheet" href="{{URL::asset('storage/css/mail.css')}}">
 </head>
 
 <body>
     <div>
-        <div class="sectionImage">
-            <img src="https://dismacapi.grazcompany.com/storage/dismac_clasic.png" class="imageDismac" />
+        <div style="width: 100%;background-color: white;">
+            <img src="https://dismacapi.grazcompany.com/storage/dismac_clasic.png" style="width: 100px;" />
         </div>
-        <div class="contentSection">
-            <div class="centerContent">
-                <div class="sizeMax">
-                    <h4 class="margin5">Número de orden: <?= $Orden->nro_proforma; ?></h4>
-                    <hr class="margin0"/>
-                    <h6 class="margin5">Fecha de realización <?= $Orden->created_at; ?></h6>
-                    <h6 class="margin5">Fecha de entrega <?= $fechaCompromiso; ?></h6>
+        <div style="width: 100%;background-color: #f5f5f5;margin-bottom: 10px;">
+            <div style="margin-left: 10%;width: 80%;background-color: white;">
+                <div style="width: 100%;">
+                    <h4 style="margin: 5px;">Número de orden: <?= $Orden->nro_proforma; ?></h4>
+                    <hr style="margin: 0px;"/>
+                    <h6 style="margin: 5px;">Fecha de realización <?= $Orden->created_at; ?></h6>
+                    <h6 style="margin: 5px;">Fecha de entrega <?= $fechaCompromiso; ?></h6>
                 </div>
-                <div class="sizeMax margin5">
-                    <p class="pTextMail"><small class="size13"><b>Nro de factura:</b> <?= $Orden->nro_factura; ?></small></p>
-                    <p class="pTextMail"><small class="size13"><b>Nro de control:</b> <?= $Orden->nro_control; ?></small></p>
+                <div style="width: 100%;margin: 5px;">
+                    <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Nro de factura:</b> <?= $Orden->nro_factura; ?></small></p>
+                    <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Nro de control:</b> <?= $Orden->nro_control; ?></small></p>
                 </div>
-                <div class="contentMaxFlex">
-                    <div class="contentColumnFlex">
-                        <h5 class="margin5">Datos de envío</h5>
-                        <div class="margin5">
-                            <p class="pTextMail"><small class="size13"><?= $Customer->nombre." ".$Customer->apellido_paterno." ".$Customer->apellido_materno; ?></small></p>
-                            <p class="pTextMail"><small class="size13"><?= $Address->AddressExtra->extra; ?></small></p>
-                            <p class="pTextMail"><small class="size13"><?= $Address->AddressExtra->address; ?></small></p>
-                            <p class="pTextMail"><small class="size13"><?= $Address->Municipio->name; ?></small></p>
-                            <p class="pTextMail"><small class="size13"><?= $Address->Ciudad->name; ?></small></p>
-                            <p class="pTextMail"><small class="size13"><?= $Address->Pais->name; ?></small></p>
+                <div style="width: 100%;display: flex;">
+                    <div style="width: 50%;display: inline-block;">
+                        <h5 style="margin: 5px;">Datos de envío</h5>
+                        <div style="margin: 5px;">
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Customer->nombre." ".$Customer->apellido_paterno." ".$Customer->apellido_materno; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Address->AddressExtra->extra; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Address->AddressExtra->address; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Address->Municipio->name; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Address->Ciudad->name; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Address->Pais->name; ?></small></p>
                         </div>
                     </div>
-                    <div class="contentColumnFlex">
-                        <h5 class="margin5">Datos de facturación</h5>
-                        <div class="margin5">
-                            <p class="pTextMail"><small class="size13"><?= $Customer->nombre." ".$Customer->apellido_paterno." ".$Customer->apellido_materno; ?></small></strong>
-                            <p class="pTextMail"><small class="size13"><?= $Customer->TipoDocumento->type; ?>: <?= $Customer->num_documento; ?></small></p>
-                            <p class="pTextMail"><small class="size13">Telf: <?= $Customer->num_telefono; ?></small></p>
+                    <div style="width: 50%;display: inline-block;">
+                        <h5 style="margin: 5px;">Datos de facturación</h5>
+                        <div style="margin: 5px;">
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Customer->nombre." ".$Customer->apellido_paterno." ".$Customer->apellido_materno; ?></small></strong>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><?= $Customer->TipoDocumento->type; ?>: <?= $Customer->num_documento; ?></small></p>
+                            <p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;">Telf: <?= $Customer->num_telefono; ?></small></p>
                         </div>
                     </div>
                 </div>
-                <div class="contentTable">
-                    <table class="sizeMax">
+                <div style="width: 100%;display: flex;margin-top: 10px;margin: 5px;">
+                    <table style="width: 100%;">
                         <thead>
-                            <tr class="sizeMax">
-                                <th colspan="2" scope="rwo"><p class="pTextMail"><small class="size13"><b>Producto</b></small></p></th>
-                                <th scope="row" class="textRight"><p class="pTextMail"><small class="size13"><b>Cantidad</b></small></p></th>
-                                <th scope="row"><p class="pTextMail"><small class="size13"><b>Precio</b></small></p></th>
+                            <tr style="width: 100%;">
+                                <th colspan="2" scope="rwo"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Producto</b></small></p></th>
+                                <th scope="row" style="text-align: right;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Cantidad</b></small></p></th>
+                                <th scope="row"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Precio</b></small></p></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -73,18 +81,18 @@ $SalesDetails = $Orden->SalesDetails;
                             foreach ($SalesDetails as $key => $Detail) {
                                 echo '<tr>
                                     <td colspan="2">
-                                        <div class="contentMaxFlex">
-                                            <div class="contentListImage">
-                                                <img src="'.$PictureApi->productFirstPicture($Detail->Product->id).'" class="imageProduct" />
+                                        <div style="width: 100%;display: flex;">
+                                            <div style="width: 60px;display: inline-block;">
+                                                <img src="'.$PictureApi->productFirstPicture($Detail->Product->id).'" style="width: 60px;height: 60px;" />
                                             </div>
-                                            <div class="contentListProduct">
-                                                <h5 class="margin5">'.$Detail->Product->name.'</h5>
-                                                <h6 class="margin5">'.$Detail->Product->sku.'</h6>
+                                            <div style="width: calc(100% - 60px);display: inline-block;">
+                                                <h5 style="margin: 5px;">'.$Detail->Product->name.'</h5>
+                                                <h6 style="margin: 5px;">'.$Detail->Product->sku.'</h6>
                                             </div>
                                         </div>
                                     </td>
-                                    <td class="textCenter"><p class="pTextMail"><small class="size13 marginRight"><b>'.$Detail->qty.'</b></small></p></td>
-                                    <td class="textCenter"><p class="pTextMail"><small class="size13"><b>'.$Detail->subtotal.'</b></small></p></td>
+                                    <td style="text-align: center;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;margin-right: 10px"><b>'.$Detail->qty.'</b></small></p></td>
+                                    <td style="text-align: center;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>'.$Detail->subtotal.'</b></small></p></td>
                                 </tr>';
                             }
                             ?>
@@ -92,34 +100,34 @@ $SalesDetails = $Orden->SalesDetails;
                         <tfoot>
                             <tr>
                                 <th colspan="2"></th>
-                                <th class="textRight"><p class="pTextMail"><small class="size13"><b>Subtotal</b></small></p></th>
-                                <th class="textCenter"><p class="pTextMail"><small class="size13"><b><?= $Orden->subtotal; ?> Bs</b></b></small></p></th>
+                                <th style="text-align: right;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Subtotal</b></small></p></th>
+                                <th style="text-align: center;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b><?= $Orden->subtotal; ?> Bs</b></b></small></p></th>
                             </tr>
                             <tr>
                                 <th colspan="2"></th>
-                                <th class="textRight"><p class="pTextMail"><small class="size13"><b>Descuentos</b></small></p></th>
-                                <th class="textCenter"><p class="pTextMail"><small class="size13"><b><?= $Orden->discount; ?> Bs</b></small></p></th>
+                                <th style="text-align: right;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Descuentos</b></small></p></th>
+                                <th style="text-align: center;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b><?= $Orden->discount; ?> Bs</b></small></p></th>
                             </tr>
                             
                             <tr>
                                 <th colspan="2"></th>
-                                <th class="textRight"><p class="pTextMail"><small class="size13"><b>Total</b></small></p></th>
-                                <th class="textCenter"><p class="pTextMail"><small class="size13"><b><?= $Orden->total; ?> Bs</b></b></small></p></th>
+                                <th style="text-align: right;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b>Total</b></small></p></th>
+                                <th style="text-align: center;"><p style="margin: 0px;line-height: 15px;"><small style="font-size: 12px;"><b><?= $Orden->total; ?> Bs</b></b></small></p></th>
                             </tr>
                         </tfoot>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="contentLinks">
-            <a href="https://www.facebook.com/DismacBolivia/" class="inlineBlock">
-                <img src="https://dismacapi.grazcompany.com/storage/fbnew.png" class="contentImage"/>
+        <div style="width: 100%;background-color: white;text-align: center;justify-content: center;">
+            <a href="https://www.facebook.com/DismacBolivia/" style="display: inline-block;">
+                <img src="https://dismacapi.grazcompany.com/storage/fbnew.png" style="width: 40px;height: 40px;border-radius: 20px;"/>
             </a>
-            <a href="https://www.instagram.com/dismacbolivia/" class="inlineBlock">
-                <img src="https://dismacapi.grazcompany.com/storage/instanew.png" class="contentImage"/>
+            <a href="https://www.instagram.com/dismacbolivia/" style="display: inline-block;">
+                <img src="https://dismacapi.grazcompany.com/storage/instanew.png" style="width: 40px;height: 40px;border-radius: 20px;"/>
             </a>
-            <a href="https://www.youtube.com/channel/UCa_XVvWTrq7C0IIq_Oiswqw/videos" class="inlineBlock">
-                <img src="https://dismacapi.grazcompany.com/storage/ytnew.png" class="contentImage"/>
+            <a href="https://www.youtube.com/channel/UCa_XVvWTrq7C0IIq_Oiswqw/videos" style="display: inline-block;">
+                <img src="https://dismacapi.grazcompany.com/storage/ytnew.png" style="width: 40px;height: 40px;border-radius: 20px;"/>
             </a>
         </div>
     </div>
