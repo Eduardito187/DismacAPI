32e9ba338e6899dbc3e17a019338a82962c4e1e0
Eduardito187 <eduardchavez302@gmail.com>
2023-05-30 00:07:29 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
     <style>
         @import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
-        @import url('css/mail.css');
     </style>
+    <link rel="stylesheet" href="{{URL::asset('css/mail.css')}}">
 </head>
 
 <body>
