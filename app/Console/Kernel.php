5492cb21dd1e7b87ef1ee2ede9609b37589e90f1
Eduardito187 <eduardchavez302@gmail.com>
2023-06-09 04:39:14 -0400

    day 18.1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('return_stock:cron')->everyMinute();
     }
 
     /**
