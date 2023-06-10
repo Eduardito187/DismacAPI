069581d7646b8ee4adfa5db14789d7973a838333
Eduardito187 <eduardchavez302@gmail.com>
2023-06-10 06:00:21 -0400

    day 18

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('return_stock:cron')->everyMinute();
+        $schedule->command('backupdb:cron')->daily();
     }
 
     /**
