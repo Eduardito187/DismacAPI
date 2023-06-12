975eaa37293a0ab477e4cffd2f2faafc881b68c8
Eduardito187 <eduardchavez302@gmail.com>
2023-06-11 22:10:09 -0400

    Update Import.php

--- app/Classes/Import/Import.php
+++ app/Classes/Import/Import.php
@@ -90,7 +90,7 @@ class Import{
     }
 
     public function validateFile(Process $Process){
-        if (Storage::exists('public/Process/1/1686298936-process-1686298936.csv')){
+        if (Storage::exists('Process/1/1686298936-process-1686298936.csv')){
             throw new Exception("Existe el archivo.");
         }else{
             throw new Exception("No existe el archivo.");
