49878370b1b32703e2deeaaf465b41f23ded60f3
Eduardito187 <eduardchavez302@gmail.com>
2023-06-18 08:36:10 -0400

    Update Import.php

--- app/Classes/Import/Import.php
+++ app/Classes/Import/Import.php
@@ -280,12 +280,10 @@ class Import{
      */
     public function validateHeadersCsv(Process $Process, array $HeaderCsv){
         if (count($HeaderCsv) > 0){
+            print_r($HeaderCsv);
             $HeaderCsv = explode($this->text->getDelimiterCode(), strval($HeaderCsv[0]));
             for ($i=0; $i < count($HeaderCsv); $i++) {
                 $code = strval($HeaderCsv[$i]);
-                print_r($code."_");
-                
-                print_r(strcmp($code, $this->text->getSku())."_");
                 if ($i == 0 && $code != $this->text->getSku()){
                     $this->errorProcess(self::ERROR_3);
                     $this->addLogHistory(self::SKU_CONTENT, $this->status->getDisable(), $this->date->getFullDate());
