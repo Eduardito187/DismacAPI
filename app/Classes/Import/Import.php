2ddbd6bc1d695435a8f050774ad97619dca77e23
Eduardito187 <eduardchavez302@gmail.com>
2023-06-18 08:27:18 -0400

    Update Import.php

--- app/Classes/Import/Import.php
+++ app/Classes/Import/Import.php
@@ -283,6 +283,7 @@ class Import{
             $HeaderCsv = explode($this->text->getDelimiterCode(), strval($HeaderCsv[0]));
             for ($i=0; $i < count($HeaderCsv); $i++) {
                 $code = strval($HeaderCsv[$i]);
+                print_r($code);
                 print_r(strcmp($code, $this->text->getSku()));
                 if ($i == 0 && $code != $this->text->getSku()){
                     $this->errorProcess(self::ERROR_3);
