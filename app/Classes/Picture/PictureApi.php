17394cae1140a86606c4beafdb9ea3c71b44457f
Eduardito187 <eduardchavez302@gmail.com>
2023-06-24 05:58:21 -0400

    Update PictureApi.php

--- app/Classes/Picture/PictureApi.php
+++ app/Classes/Picture/PictureApi.php
@@ -101,46 +101,23 @@ class PictureApi{
     }
 
     public function processZipFile(){
-        $id_figura = 11;
-        // Obtienes los subdirectorios que están dentro del directorio del cliente
-        //$directorios_del_cliente = Storage::directories("/storage/Process/1/1687598029-picture-1687598029/");
+        $id_figura = "Imagenes";
         $directorios_del_cliente = Storage::directories(str_replace("storage", "public", "storage/Process/1/1687598029-picture-1687598029/"));
-        print_r("HOLA");
         print_r($directorios_del_cliente);
-        /*
-        // creas un array vacío para ir llenándolo con los elementos año/mes/archivos
-        $tree_array = [];
-
-        // iteras sobre los directorios obtenidos
-        foreach ($directorios_del_cliente as $directorio_del_ano) {
-            // haciendo un explode, separas el string en un array con cada directorio.
-            $temp_array = explode('/', $directorio_del_ano);
-            // obtienes el último elemento (el año).
-            $year = end( $temp_array );
-            // Obtienes los subdirectorios que están dentro del directorio del año
-            $subdirectorios_del_ano = Storage::directories("/public/Process/1/$id_figura/$year");
+        
+        foreach ($directorios_del_cliente as $dir) {
+            $temp_array = explode('/', $dir);
+            $year = end($temp_array);
+            $subdirectorios_del_ano = Storage::directories("storage/Process/1/1687598029-picture-1687598029/$id_figura/$year");
             foreach ($subdirectorios_del_ano as $directorio_del_mes) {
                 $temp_array = explode('/', $directorio_del_mes);
                 // obtienes el último elemento (el mes).
-                $month = end( $temp_array );
+                $filename = end( $temp_array );
                 // Obtienes los archivos que están dentro del directorio del mes
                 $archivos_del_mes = Storage::files($directorio_del_mes);
-                foreach ($archivos_del_mes as $archivo_del_mes) {
-                    $temp_array = explode('/', $archivo_del_mes);
-                    // obtienes el último elemento (el nombre del archivo).
-                    $filename = end( $temp_array );
-                    // obtienes la url que corresponde a ese archivo.
-                    $url = Storage::url($archivo_del_mes);
-                    // guardas en tu array el nombre y la url del archivo, 
-                    // haciendo que el array sea multidimensional por año y mes.
-                    $tree_array[$year][$month][] = [
-                        'filename' => $filename,
-                        'url' => $url
-                    ];
-                }
+                print_r($archivos_del_mes);
             }
         }
-        */
     }
 
     /**
