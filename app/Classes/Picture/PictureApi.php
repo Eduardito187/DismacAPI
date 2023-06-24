42d37b57937dca64bfa28fae68b9671d19f20471
Eduardito187 <eduardchavez302@gmail.com>
2023-06-24 06:06:50 -0400

    Update PictureApi.php

--- app/Classes/Picture/PictureApi.php
+++ app/Classes/Picture/PictureApi.php
@@ -101,15 +101,14 @@ class PictureApi{
     }
 
     public function processZipFile(){
-        $id_figura = "Imagenes";
-        $directorios_del_cliente = Storage::directories(str_replace("storage", "public", "storage/Process/1/1687598029-picture-1687598029/"));
+        $directorios_del_cliente = Storage::directories(str_replace("storage", "public", "storage/Process/1/1687598029-picture-1687598029"));
         print_r($directorios_del_cliente);
         
         foreach ($directorios_del_cliente as $dir) {
             $temp_array = explode('/', $dir);
             $year = end($temp_array);
             print_r($year);
-            $subdirectorios_del_ano = Storage::directories(str_replace("storage", "public", "storage/Process/1/1687598029-picture-1687598029/$id_figura/$year/"));
+            $subdirectorios_del_ano = Storage::directories($dir);
             print_r($subdirectorios_del_ano);
             foreach ($subdirectorios_del_ano as $directorio_del_mes) {
                 $temp_array = explode('/', $directorio_del_mes);
