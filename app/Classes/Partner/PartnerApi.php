31528cbb9ef85966b0760e8010b21d46f48f5010
Eduardito187 <eduardchavez302@gmail.com>
2023-07-02 04:56:27 -0400

    Update PartnerApi.php

--- app/Classes/Partner/PartnerApi.php
+++ app/Classes/Partner/PartnerApi.php
@@ -132,7 +132,7 @@ class PartnerApi{
      */
     public function deletePicture(Request $request){
         unlink(storage_path()."/app/public/Products/1/1688279516-picture-1688279516.jpg");
-        $deletedFile = Storage::delete("storage/app/public/Products/1/1688279516-picture-1688279516.jpg");
+        //$deletedFile = Storage::delete("storage/app/public/Products/1/1688279516-picture-1688279516.jpg");
         return true;
         $id_Partner = $this->getPartnerByAccountId($this->getAccountToken($request->header($this->text->getAuthorization())));
         $params = $request->all();
