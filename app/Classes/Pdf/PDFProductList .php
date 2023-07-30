bf58c2e5daa3f70a9d72ff6bc9da57a638dbc3b5
Eduardito187 <eduardchavez302@gmail.com>
2023-07-30 03:26:25 -0400

    Update PDFProductList .php

--- app/Classes/Pdf/PDFProductList .php
+++ app/Classes/Pdf/PDFProductList .php
@@ -44,22 +44,21 @@ class PDFProductList extends TCPDF{
      */
     private function writeProducts(array $products){
         foreach ($products as $product) {
-            $this->Cell(0, 10, 'Producto: ' . $product[Text::COLUMN_NAME], 0, 1);
-            $this->Cell(0, 10, 'Descripción: ' . $product[Text::BRAND], 0, 1);
+            $this->Cell(0, 10, $product[Text::COLUMN_NAME], 0, 1);
+            if (!is_null($product[Text::BRAND])){
+                $this->Cell(0, 10, $product[Text::BRAND], 0, 1);
+            }
 
             // Si el precio especial es mayor a 0, tacha el precio.
             if ($product[Text::SPECIAL_PRICE] > 0) {
-                $this->SetFont('', 'S'); // Establece el estilo de fuente para tachar el texto.
-                $this->Cell(0, 10, 'Precio: $' . $product[Text::PRICE], 0, 1, 'T');
-                $this->SetFont('', ''); // Restablece el estilo de fuente al predeterminado.
-                $this->Cell(0, 10, 'Precio Especial: $' . $product[Text::SPECIAL_PRICE], 0, 1);
+                $this->SetFont('', 'S');
+                $this->Cell(0, 10, 'Bs '.$product[Text::PRICE], 0, 1, 'T');
+                $this->SetFont('', '');
+                $this->Cell(0, 10, 'Bs '.$product[Text::SPECIAL_PRICE], 0, 1);
             } else {
-                $this->Cell(0, 10, 'Precio: $' . $product[Text::PRICE], 0, 1);
+                $this->Cell(0, 10, 'Bs '.$product[Text::PRICE], 0, 1);
             }
-
             $this->Ln(5);
-
-            // Puedes ajustar las dimensiones y la disposición de las imágenes según tus necesidades.
             $imageWidth = 50;
             $imageHeight = 50;
             $this->Image($product[Text::IMAGE], $this->GetX(), $this->GetY(), $imageWidth, $imageHeight);
