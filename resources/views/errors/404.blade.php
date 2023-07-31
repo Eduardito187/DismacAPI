10c660c0f223260ededb810b5649df8259a8b4a4
Eduardito187 <eduardchavez302@gmail.com>
2023-07-31 03:50:07 -0400

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,57 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Error 404 - Página no encontrada</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="error-message">
+                <h1>¡Error 404!</h1>
+                <p>La página que estás buscando no pudo ser encontrada.</p>
+            </div>
+            <div class="animated-bg">
+                <!-- Aquí puedes agregar tu animación o efecto -->
+                <!-- Por ejemplo, puedes usar CSS animations o librerías como Animate.css -->
+            </div>
+        </div>
+    </body>
+</html>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+    }
 
-@section('content')
-    <h1>¡Error 404!</h1>
-    <p>Lo sentimos, la página que estás buscando no pudo ser encontrada.</p>
-@endsection
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .error-message {
+        text-align: center;
+        color: #333;
+    }
+
+    .error-message h1 {
+        font-size: 4rem;
+        margin-bottom: 0.5rem;
+    }
+
+    .error-message p {
+        font-size: 1.5rem;
+        margin: 0;
+    }
+
+    .animated-bg {
+        width: 200px;
+        height: 200px;
+        background-color: #ff0000;
+    }
+</style>
