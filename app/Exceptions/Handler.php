d8d39801bf73e1a5d16ce0e876b28eae495b4b3c
Eduardito187 <eduardchavez302@gmail.com>
2023-08-01 02:17:11 -0400

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -52,6 +53,9 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception){
         if ($exception instanceof NotFoundHttpException) {
             return response()->view('errors.404', [], 404);
+        }
+        if ($exception instanceof HttpException && $exception->getStatusCode() == 500) {
+            return response()->view('errors.500', [], 500);
         }
 
         return parent::render($request, $exception);
