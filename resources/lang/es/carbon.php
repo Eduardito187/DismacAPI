f250dceebc32f62886c760bb805a54c61c55005a
Eduardito187 <eduardchavez302@gmail.com>
2023-08-03 03:52:23 -0400

    2

--- resources/lang/es/carbon.php
+++ resources/lang/es/carbon.php
@@ -1,11 +1,10 @@
+<?php
 return [
-    'days' => [
-        'sunday' => 'Domingo',
-        'monday' => 'Lunes',
-        'tuesday' => 'Martes',
-        'wednesday' => 'Miércoles',
-        'thursday' => 'Jueves',
-        'friday' => 'Viernes',
-        'saturday' => 'Sábado',
-    ],
+    'sunday' => 'Domingo',
+    'monday' => 'Lunes',
+    'tuesday' => 'Martes',
+    'wednesday' => 'Miércoles',
+    'thursday' => 'Jueves',
+    'friday' => 'Viernes',
+    'saturday' => 'Sábado',
 ];
