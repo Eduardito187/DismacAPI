28520b9bd67baff52a3765f96a5f4a2926d4ccc3
Eduardito187 <eduardchavez302@gmail.com>
2023-08-03 22:01:20 -0400

    Update PluginOrder.php

--- app/Plugins/PluginOrder.php
+++ app/Plugins/PluginOrder.php
@@ -10,6 +10,7 @@ class PluginOrder{
     const TYPE_ANALYTICS = "Order";
     const STATUS_CANCEL = 1;
     const ORDER_STATUS_CANCEL = "ORDER_STATUS_CANCEL";
+    const ORDER_CREATED = "ORDER_CREATED";
     const STATUS_SUCCESS = 3;
     const ORDER_STATUS_SUCCESS = "ORDER_STATUS_SUCCESS";
     /**
@@ -22,7 +23,7 @@ class PluginOrder{
     }
 
     public function creating(Sales $model){
-        //
+        $this->Analytics->registerAnalytics(null, null, self::TYPE_ANALYTICS, self::ORDER_CREATED, $model->id, $model->total, $model->id_partner);
     }
 
     public function updated(Sales $model){
