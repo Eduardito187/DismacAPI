46eb685cdb98e4fe017dadc670d303117f211fbb
Eduardito187 <eduardchavez302@gmail.com>
2023-08-06 00:48:28 -0400

    Update Notifications.php

--- app/Classes/Helper/Notifications.php
+++ app/Classes/Helper/Notifications.php
@@ -17,22 +17,24 @@ class Notifications{
     }
 
     public function sendNotificationAndroid(){
-        $fcmEndpoint = 'https://fcm.googleapis.com/fcm/send';
+        $fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/modalidad-682cf/messages:send';
         
         $tokens = ['e6jAm2RBRhirK4-HSfljnd:APA91bHdU3JEwX0TlHVN6zsFcNQUgRhimYKFePq8RTMZ3i56mOE4ViC_IN6pznbn8cFaHille6wceDh1C1xJJLFGDp9eO6EwsbE9qu7sSGj1_sDQPp-lwfltQvdeApv4oGEUHbs6T79o'];
         
         $notification = [
-            'title' => 'Título de la notificación',
-            'body' => 'Cuerpo de la notificación'
+            'message' => [
+                'token' => $tokens[0],
+                'notification' => [
+                    'title' => 'Título de la notificación',
+                    'body' => 'Cuerpo de la notificación',
+                ],
+            ],
         ];
         
         $response = Http::withHeaders([
-            'Authorization' => 'key=' . self::SERVER_KEY_DE_FCM,
+            'Authorization' => 'Bearer ' . self::SERVER_KEY_DE_FCM,
             'Content-Type' => 'application/json',
-        ])->post($fcmEndpoint, [
-            'registration_ids' => $tokens,
-            'notification' => $notification,
-        ]);
+        ])->post($fcmEndpoint, $notification);
 
         print_r($response);
     }
