9bb13bda99be67561da321b543d6431c0bab2ba9
Eduardito187 <eduardchavez302@gmail.com>
2023-08-06 01:10:06 -0400

    Update Notifications.php

--- app/Classes/Helper/Notifications.php
+++ app/Classes/Helper/Notifications.php
@@ -5,21 +5,18 @@ namespace App\Classes\Helper;
 use App\Classes\Helper\Text;
 use Illuminate\Support\Facades\Http;
 
-class Notifications
-{
+class Notifications{
     const SERVER_KEY_DE_FCM = "1244a5d2c0b4704753057b3ff2e16f81b3911a9d";
     /**
      * @var Text
      */
     protected $text;
 
-    public function __construct()
-    {
+    public function __construct(){
         $this->text  = new Text();
     }
 
-    function getTokenAssertion($privateKey, $clientId)
-    {
+    function getTokenAssertion($privateKey, $clientId){
         $accessTokenEndpoint = 'https://www.googleapis.com/oauth2/v4/token';
         $now = time();
         $expiration = $now + 3600;
@@ -44,8 +41,7 @@ class Notifications
         return "$header.$payload.$signature";
     }
 
-    private function getAccessToken()
-    {
+    private function getAccessToken(){
         $accessTokenEndpoint = 'https://www.googleapis.com/oauth2/v4/token';
         $privateKey = '-----BEGIN PRIVATE KEY-----';
         $privateKey .= 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDDJ9FuASgsq0p1';
@@ -82,8 +78,7 @@ class Notifications
         $accessToken = $response->json()['access_token'];
     }
 
-    public function sendNotificationAndroid()
-    {
+    public function sendNotificationAndroid(){
         $fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/project-127322741666/messages:send';
 
         $tokens = ['e6jAm2RBRhirK4-HSfljnd:APA91bHdU3JEwX0TlHVN6zsFcNQUgRhimYKFePq8RTMZ3i56mOE4ViC_IN6pznbn8cFaHille6wceDh1C1xJJLFGDp9eO6EwsbE9qu7sSGj1_sDQPp-lwfltQvdeApv4oGEUHbs6T79o'];
