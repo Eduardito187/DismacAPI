6d6788a8d98030962482f21dcb468df3ecfa30f9
Eduardito187 <eduardchavez302@gmail.com>
2023-08-06 23:19:39 -0400

    Update Notifications.php

--- app/Classes/Helper/Notifications.php
+++ app/Classes/Helper/Notifications.php
@@ -43,8 +43,8 @@ class Notifications{
 
     private function getAccessToken(){
         $accessTokenEndpoint = 'https://www.googleapis.com/oauth2/v4/token';
-        $privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCbRDoLP4S3e1aW\nJtn1yJaFh5tkiIXAyZiUr9WKIqGLM2fXRjz+zXgNU58f6XJxrrUxC1zW2B0yBab0\nG0Yt4JglVDYeMrstxtacRW5eyb9fTTdBnPEoLnjaggOGnRscad247yoqLKKvIZZq\nwY+ZHYsrRXZYjhIu3PgI0V/jQMxH1ILWnC2fqGl+VF+OgnCtzUbi+/rNol/Be25q\n+xRzpwHXCqR3WAB5CezkcGncrSj1nmCNT0zfPLDo4jtL7M9EqiqLCED+ofoLMv8A\nfiNsRv2G1b0F7oaaz+jg3Gy/KD374HMthNI5y9YHA7nKhx2sxiVCc5YQsrfDXSac\nn0rk4uiHAgMBAAECggEALuTEyRfmjAn2xUO3qZlrCKpDvvHSv77DVAjlLCK4epPM\nhYKjsms5GoFKendlxrr243ikkgjUjIKTgX9pSzrdiMytyaIV0UYQFEOOCZfSMkTe\nbvUpCrM6cfg5e2MgyIya2Bt3tyQjAFTbGqQaLCr1mNySWT2TdU7spLEjzj2IVRr7\n5QzIUmBfyFDNIV9gftWHZ6N8I0y5u2AnbgklvJAPOfTPqcz1P+4I9ti+3I846jDv\nppeuNoeClJcdswJe50hVSoiIcs9xWoACh8fkBdPuh+qcbEaaZzljWil7JGYYDHQS\nW10SxI2wZ2HrNeZOFViz6zr5wtGmYeRoVbgftaF8aQKBgQDOvVK7LzsJq8xcXvYQ\njGZ6ozPKr2CMDIS3bW6A0uuPpPJ+8sjj/jwnwbZR5RcUW66I2E9GR5MYv2UR2GRn\nZf8wDMLs8JzxfwI+BxXCpHnNL4GZqXaIyKbPlp4dHI2GE6VHc28NiXrLGGWXqeT4\nqPXs9DNTJcDDdBuHxOAgZ/2ezwKBgQDAQygtWkUBtYOJw2/R6Rn+0tZ2h/pHOlry\nHr+fMA7P2ov1WGv4XT85dHh8TTSHkzuXBQJX5yLdQhlRVjLbpHNyh/kmaGnfwy+E\nrKD4plSB/72r4fSXRET2jmy5waRNhY9CuuNH0fuukPmG5sgNkiZT+KvRGvL5Nfr+\nc8pZEjlIyQKBgQCju82J8h4PysBPZ+X5tTh0aEzceYk9Yay6mTCrVmC5oylq4V25\nM+Dwm7qJoaZluTbBqo8eWhFlyC+Dsp3SRjWVGIZIoUeq+6wK9BGH1juhBb7etBfs\nvP1f6ynOZI6xYV3E+Oc7EWbu8qODOkrdpPMgd0cu0veX1Jz5KjNXdwSmgQKBgF/W\n2kdJUU7Zybk5uBr+RNwXA5tQiz1IZJ9/HmxfC3MncU8bBa9n5CVi3tCvt7jBdxoA\nADwLVwDOe0plWrLMllQIXfV8ZRKK/Lv5RcDNQSEQd98fospo2KvDMYWjdqDCLDjK\napOZpAlP8WMC1cSWDw8azaNN+MKr2vNOixa9k9qpAoGAZv2B3Wgi7g+DPvJjp+v0\nTBVJigLkhlPRMLqc+KLmDdDQdCSA+cMvNch6tA6SQ4zanUVZdOrTI71KtsH04C9+\n0Hrpq4q5xMPWuIT7OVDVJ9mWl1eccksjai/4pWG1vt/H3q6/jnTmxiGNqvLxArPc\nXc1wRWL7t1hIAagBL6F7Ndc=\n-----END PRIVATE KEY-----\n";
-        $clientId = "fyz-3-754@modalidad-682cf.iam.gserviceaccount.com";
+        $privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCxF48gnU8byUB5\nrEZSk/9/hvZ0uLqe2i6qb/tKFa7I/TV0YhaA6X0eAselXjx5ERHA9RQiZVSJGRrJ\n/S6vclGHWT6v7KQZxGByoRAXq445YNnbErk42XwxMH4JcZ1zwh8J/28nKjQYktX1\n+OZtaHliQ6mbnpQilnEUhNWNOHwND2BNdi+GHjOy4i4aJoK1l/KsC4CP1xTu5SKm\nMWq4w2Sj0T1dVqUEP/sjiLZXtXZ9UrHpgMdJ3g/nWef7TW+B1NCZqucVoNU5SyhX\nNIqkCmWwdxifvsE8e5F2D9mEkCIiC0TOWrdgVmuqB3iuJ+WL0EutzcSoxMpB/IOy\nUqUmnExNAgMBAAECggEADvcOMx6IBQbslLugVQceWVusL7CSo1eNslJ0j6NQb2Sn\nuUDhZPMWOHcvVtlP+UJzgnT+HBVTCk4y9ixtNF9P5hpTvVByjPOMMPOiZVWr1vZz\nwoW3svGnWI11Vn1MOxra6PXgdz9OQrYEhbI1tWYq58kO/VBudT51qjBleXcd/d5m\nxeU3ViggHYSFTYq1Ehxg5Ux6GXLLiRONqh1hOhQKMvR+w48SRkjvOqRIIWal93vS\n8XBLb/GROAmb1jgR/DjTtWubVikJH6Wmw97K0eztUQpfSHpbRgvVexgPYeK0q//Q\nmooavmMiSwsZNp+yjOCR6tbZ5ViIYqHyFRj8brISIwKBgQDrOuqABmyXCwz4UCCm\nod06FPbo5BVKmeuxee4KF7TyLyX+exPiSnm3s5lS8RWGobh964Pt4aB6QQFbWp+o\nF+NYKCBsdpninzQGPV+pP4ekMyJQoF4d656hYgU5umWUjQYy7DFdgGjbKyDrsxMK\n+sq5xw+g1yTgu3cJWD7lU53Z1wKBgQDAuoDni14GbyW77iebL57gyiwcMV3adHME\na+uUu+aQw5K8hxkQ4/u8rlp0hsDgzjrgMqBpz05VYJHrgwO7Zbb0B3i1iaGKax7+\n1pJubfHQmU3FXKr4lPB3a3dEyJUyzYHJeLSov/QweJyAlwo6vb8MWs7moaGemmAA\nvYyr9GAuewKBgC+GisRtB9mIgQlWXxJrl5/Zhn1NP8P/zy/fW6msWoGn5vxAb6cD\ntw3Xen3Yeanm2LiQMeqI8Hxiz9xeNe2nTeaMzOg9GiBAXCl6ku5GCKizMBasH/fZ\nmAYxIK8mKsnVqE/3io3CmOzXDOKyHoHUY8sfHdg4P5osJgO8UZDR0Q2nAoGAR3pk\nKHvlyVkbk5GGGuYUdKZGqcYdWtHpfnBQFP+DSZlfKJCqWTtUt+uqHKGFk1qpN1FP\ncS6zjLKeK8yRa8UTj3nd6OY8hUupWTZXZKFeF9FjqJjvx/XlIUU4HMiPnSHeysv7\npMJcXEW/NWnPH24UFw7uwVaczGfQxzWFoJinu1UCgYBOQOphykAEix+HhZRwB5N+\niqVADnrsGIppNUEWNtL24+sGGLIhiYbEVxbwikqzF1WRFyPkT9UHrLsiIn98svaM\nAjzR/SoIisa/HCXI9pi0oLZ+cMOhI5QysjM9qOCQ59W0NQbaIoQknB7jYtkuctry\nfAWOCiohJVnkcKeODU427A==\n-----END PRIVATE KEY-----\n";
+        $clientId = "notificaciones-a60ac@appspot.gserviceaccount.com";
 
         $response = Http::post($accessTokenEndpoint, [
             'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
@@ -55,6 +55,7 @@ class Notifications{
     }
 
     public function sendNotificationAndroid(){
+        /*
         $serverKey = 'AAAAHaUG26I:APA91bFDOcLu0wTpRsR7PsFoCJJIqYnwh3l6yJBs239HqzZtrWWq7JhzP2SRQQRHOvLeTFSPqfzwgpN7sHcJH8QgETL8u9DnWCCdLWuI-aFFMbT5T5RwbNgp-NJXf-vZXSuGssvvBtrT';
         $fcmEndpoint = 'https://fcm.googleapis.com/fcm/send';
 
@@ -73,13 +74,13 @@ class Notifications{
             'notification' => $notification,
         ]);
         print_r($response);
-        /*
+        */
 
-        $fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/modalidad-682cf/messages:send';
+        $fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/notificaciones-a60ac/messages:send';
 
         $notification = array(
             "message" => array(
-                "token" => 'e6jAm2RBRhirK4-HSfljnd:APA91bHdU3JEwX0TlHVN6zsFcNQUgRhimYKFePq8RTMZ3i56mOE4ViC_IN6pznbn8cFaHille6wceDh1C1xJJLFGDp9eO6EwsbE9qu7sSGj1_sDQPp-lwfltQvdeApv4oGEUHbs6T79o',
+                "token" => 'fofveZuBRLuskqi6YuuPvS:APA91bHN9_iwToKLq6AdvhOcGO0K3sUzhA8X_bEf6qj5UCimtV5FpD91Bs4WCVYxprAnVua904-ApZY-jrOpJQfpOCrK10HWvwEfen62B4VWj4X1f73C3tFjy51_YCFHUb7Fl-kGiHu-',
                 "notification" => array(
                     "title" => "Match update",
                     "body" => "Arsenal goal in added time, score is now 3-0"
@@ -113,7 +114,7 @@ class Notifications{
             'Authorization' => 'Bearer '.$this->getAccessToken(),
             'Content-Type' => 'application/json;charset=UTF-8'
         ])->post($fcmEndpoint, $notification);
-        */
+        
         
     }
 }
