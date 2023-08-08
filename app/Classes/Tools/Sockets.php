c9459672dc1b2ffa0b1b77d7ec348ddc5b8bf539
Eduardito187 <eduardchavez302@gmail.com>
2023-08-08 02:12:00 -0400

    Update Sockets.php

--- app/Classes/Tools/Sockets.php
+++ app/Classes/Tools/Sockets.php
@@ -32,6 +32,7 @@ class Sockets{
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         curl_close($ch);
     }
