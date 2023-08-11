83facbb4f098a86d6fdb5e25021903266a1f608e
Eduardito187 <eduardchavez302@gmail.com>
2023-08-10 22:37:53 -0400

    Update Sockets.php

--- app/Classes/Tools/Sockets.php
+++ app/Classes/Tools/Sockets.php
@@ -49,7 +49,20 @@ class Sockets{
      * @return void
      */
     public function sendNotification($token, $title, $body){
-        $http = Http::post(self::FCM, [self::TO => $token, self::NOTIFICATION => array('body' => $body, 'title' => $title)])->header(self::TYPE_JSON);
+        $ch = curl_init(self::FCM);
+        $data = [
+            self::TO => "fXZ0N3oxS36-fIpKvuXdK1:APA91bFmg5VzP2mG0JSuaJ4IfBKtNr0zHBqmL9eb1iaFb9K0-2GcXlB6-SAillsx6odJpwViG9XKb7Xi6saGkxuAjn8u88MwxV1K2w9zk_XQ_gMpWlmWzpLgj2hF_3RkvZEMGW4gtsNY",
+            self::NOTIFICATION => [
+                "body" => "Revirtamos longtime?",
+                "title" => "PlatformDismac",
+            ],
+        ];
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(self::TYPE_JSON));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
     }
 }
 ?>
